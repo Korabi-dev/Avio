5b3237dd7dc4709159d2354b614636e2d2ae9a28
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-03 00:18:54 +0200

    Create hello.js

---
+++ commands/moderation/hello.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js")
+
+
+module.exports = {
+  name: "nsuggest",
+  usage: "suggest <message>",
+  description: "Send your Suggestion",
+  category: "main",
+  run: (client, message, args) => {
+    
+    if(!args.length) {
+      return message.channel.send("Please Give the Suggestion")
+    }
+    
+    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "name-suggestions"))
+    
+    
+    if(!channel) {
+      return message.channel.send("there is no channel with name - name-suggestions")
+    }
+                                                    
+    
+    let embed = new MessageEmbed()
+    .setAuthor("NAME_SUGGESTION: " + message.author.tag, message.author.avatarURL())
+    .setThumbnail(message.author.avatarURL())
+    .setColor("#ff2050")
+    .setDescription(args.join(" "))
+    .setTimestamp()
+    
+    
+    channel.send(embed).then(m => {
+      m.react("✅")
+      m.react("❌")
+    })
+    
+
+    
+    message.channel.send("Sent Your Suggestion to " + `${channel}` )
+    
+  }
+}
