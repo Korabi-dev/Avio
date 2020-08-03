25cd8d2786cf9f878d002d1a047ab7dec0603bd9
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-03 19:55:52 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,7 +9,7 @@ module.exports = {
   run: (client, message, args) => {
    let embed = new discord.MessageEmbed()
     .setTitle("Commands:")
-    .setDescription(` Prefix,8ball,ad,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss.`)
+    .setDescription(` Prefix,8ball,ad,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss,membercount.`)
     .setColor("#ff2050")
     .setFooter(`Made by Korabi20#0001 and owned by glitch!#3950`);
     
