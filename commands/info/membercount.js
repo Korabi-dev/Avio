8ff08590db99d4cb297d40d8d831c5239e4505d2
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-03 19:52:20 +0200

    Create membercount.js

---
+++ commands/info/membercount.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "membercount",
+  category: "info",
+  description: "A simple command that tells how manymembers are in the guild",
+  usage: "membercount",
+  run: (client, message, args) => {
+  message.channel.send(`There are **${message.guild.memberCount}** in **${message.guild.name}**!`) 
+}}
