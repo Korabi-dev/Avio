2ca224310f14b4761c8a95d6309012dfcf13fabf
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-06 16:40:19 +0200

    Create info.js

---
+++ commands/info/info.js
@@ -0,0 +1,18 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "info",
+  category: "info",
+  aliases: ['commands', 'info', 'information', 'cmds', 'cmd'],
+  description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+   let embed = new discord.MessageEmbed()
+    .setTitle("Commands:")
+    .setDescription(` Prefix,8ball,ad,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss,membercount,pat.`)
+    .setColor("#ff2050")
+    .setFooter(`Made by Korabi20#0001 and owned by glitch!#3950`);
+    
+    message.channel.send(embed)
+}}
