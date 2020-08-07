f4c076c75e2d3c8d039955be52a79d9357b500db
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-07 13:18:45 +0200

    Create status.js

---
+++ commands/info/status.js
@@ -0,0 +1,11 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "status",
+  category: "info",
+  description: "get the status",
+  usage: "status",
+  run: (client, message, args) => {
+ message.channel.send('Bot status: Online and well!')
+}}
