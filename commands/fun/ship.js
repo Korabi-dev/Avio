c9c6ae411bbc6c1d8974a1e6d5c41a90a10679d2
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-08 23:17:59 +0200

    Create ship.js

---
+++ commands/fun/ship.js
@@ -0,0 +1,32 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "ship",
+  category: "fun",
+  description: "measure your love!",
+  usage: "ship <@user>",
+  run: (client, message, args) => {
+
+let target = message.mentions.members.first();
+
+if(!target) {
+
+return message.channel.send('Please mention someone!')
+
+}
+
+let response = (`${math.random[0,100]}`)
+
+  let embed = new discord.MessageEmbed()
+  .setTitle('LOVE`)
+  .setDescription(`${message.author} is {response}% in love with ${target}`)
+  .setColor("RED')
+  
+  
+
+
+
+
+
+}
+}
