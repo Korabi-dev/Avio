91f5a4e1b810584a9fa90b403edd283d8c2c430b
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-09 00:04:04 +0200

    Update commands.js

--- commands/info/commands.js
+++ commands/info/commands.js
@@ -1,16 +1,14 @@
-  
 const discord = require("discord.js");
 
 module.exports = {
   name: "commands",
-  category: "info",
-  aliases: ['commands', 'info', 'information', 'cmds', 'cmd'],
-  description: "get the commands",
+   category: "info",
+   description: "get the commands",
   usage: "help",
   run: (client, message, args) => {
    let embed = new discord.MessageEmbed()
     .setTitle("Commands:")
-    .setDescription(` Prefix,8ball,ad,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss,membercount,pat.`)
+    .setDescription(` Prefix,8ball,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss,membercount,pat,ship.`)
     .setColor("#ff2050")
     .setFooter(`Made by Korabi20#0001 and owned by glitch!#3950`);
     
