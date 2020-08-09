89097883094105ab5957c8177bdbf81c945cf967
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-09 19:22:38 +0200

    Create avatarcheck.js

---
+++ commands/fun/avatarcheck.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, Discord } = require("discord.js");
+module.exports = {
+  name: "announce",
+  description: "Get the bot to say what ever you want in a specific channel.",
+  usage: "<channel id> <msg>",
+  run: async (bot, message, args) => {
+  
+  
+  
+  if(!target){
+return message.channel.send('Please mention someone!')
+}
+  
+  
+
+  
+      let Embed = new MessageEmbed()
+        .setTitle(`AVATAR`)
+        .setDescription(`${target}'s avatar`)
+        .setImage(traget.user.displayAvatarURL())
+        .setColor(`RANDOM`);
+      message.channel.send(Embed);
+  
+  
+  
+  
+  
+  
+  }
