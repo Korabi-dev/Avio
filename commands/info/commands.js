a0e9ffb404414cd1f46af2b9ab787c43f412ea03
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-11 17:14:02 +0200

    Update commands.js

--- commands/info/commands.js
+++ commands/info/commands.js
@@ -10,7 +10,7 @@ module.exports = {
     .setTitle("Commands:")
     .setDescription(` Prefix,8ball,announce,whois,dm,giveaway,meme,poll,suggest,reddit,say,snipe,trivia,oldest,ping,findalt,report,warn,mute,kick,warns,ban,role,emoji,timer,support,    invite,slowmode,embedify,kill,pp,hug,kiss,membercount,pat,ship,av.`)
     .setColor("#ff2050")
-    .setFooter(`Made by Korabi20#0001 and owned by glitch!#3950`);
+    .setFooter(`Made by Korabi20#8725 and owned by glitch!#3950`);
     
     message.channel.send(embed)
 }}
