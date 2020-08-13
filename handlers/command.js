b10d814d3f987f9f50518c248f9975a37452aa3f
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-13 21:16:14 +0200

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,16 +1,14 @@
-const { Client, Collection } = require('discord.js');
-
-module.exports = class extends Client {
-	constructor(config) {
-		super({
-			disableEveryone: true,
-			disabledEvents: ['TYPING_START'],
-		});
-
-		this.commands = new Collection();
-
-		this.queue = new Map();
-
-		this.config = config;
-	}
+  
+const { readdirSync } = require("fs");
+module.exports = (bot) => {
+  readdirSync("./commands/").map((dir) => {
+    const commands = readdirSync(`./commands/${dir}/`).map((cmd) => {
+      let pull = require(`../commands/${dir}/${cmd}`);
+      console.log(`Loaded command ${pull.name}`);
+      bot.commands.set(pull.name, pull);
+      if (pull.aliases) {
+        pull.aliases.map((p) => bot.aliases.set(p, pull));
+      }
+    });
+  });
 };
