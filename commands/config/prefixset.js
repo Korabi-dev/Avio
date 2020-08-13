a3ca242ee4bcd974d2726dc3144ce580d409cf0b
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-14 00:56:35 +0200

    Update prefixset.js

--- commands/config/prefixset.js
+++ commands/config/prefixset.js
@@ -4,29 +4,8 @@ module.exports = {
   description: "Set the prefix of the guild!",
   category: "config",
   run: async (bot, message, args) => {
-    
-    
-       if(message.author.id === '638476135457357849'){
-if (!args[0])
-      return message.channel.send(
-        `The prefix for ${message.guild.name} is \`${
-          (await db.get(`Prefix_${message.guild.id}`))
-            ? await db.get(`Prefix_${message.guild.id}`)
-            :  "r!"
-        }\``
-      );
-    await db.set(`Prefix_${message.guild.id}`, args[0]);
-    message.channel.send(`Set the prefix to \`${args[0]}\`!`);
-     
-     return console.log(`${message.author.username} just changed the prefix!`)
-  }
-}
-}
-    
-    
-    
-    if (!message.member.permissions.has("ADMINISTRATOR")) {
-return message.channel.send(`${message.author.username} you need administrator premissions to use this command.`)
+ if (!message.member.permissions.has("BAN_MEMBERS")) {
+return message.channel.send(`${message.author.username} you need more premissions to use this command.`)
 }
     
 if (!args[0])
