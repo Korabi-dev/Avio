43f556af3d501229618b009d4866b0f7daf418f8
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-14 00:54:54 +0200

    Update prefixset.js

--- commands/config/prefixset.js
+++ commands/config/prefixset.js
@@ -4,11 +4,9 @@ module.exports = {
   description: "Set the prefix of the guild!",
   category: "config",
   run: async (bot, message, args) => {
-    if (!message.member.permissions.has("ADMINISTRATOR")) {
-return message.channel.send(`${message.author.username} you need administrator premissions to use this command.`)
-}
     
-   if(message.author.id === '638476135457357849'){
+    
+       if(message.author.id === '638476135457357849'){
 if (!args[0])
       return message.channel.send(
         `The prefix for ${message.guild.name} is \`${
@@ -21,12 +19,17 @@ if (!args[0])
     message.channel.send(`Set the prefix to \`${args[0]}\`!`);
      
      return console.log(`${message.author.username} just changed the prefix!`)
-  },
-};
-  return console.log(`${message.author.username} just changed the prefix!`)
   }
+}
+}
     
-    if (!args[0])
+    
+    
+    if (!message.member.permissions.has("ADMINISTRATOR")) {
+return message.channel.send(`${message.author.username} you need administrator premissions to use this command.`)
+}
+    
+if (!args[0])
       return message.channel.send(
         `The prefix for ${message.guild.name} is \`${
           (await db.get(`Prefix_${message.guild.id}`))
