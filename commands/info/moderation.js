e6a0d5b77833e87e058d96963590e04215f7732d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 11:53:40 +0200

    Update moderation.js

--- commands/info/moderation.js
+++ commands/info/moderation.js
@@ -14,6 +14,6 @@ module.exports = {
     .setFooter(`Made by Korabi20#8725 and owned by glitch!#3950`);
     
    
-    message.channel.send(`Moderation commands sent! :white_check_mark:`)
+    message.channel.send(`Moderation Commands Sent! :white_check_mark:`)
     message.author.send(embed)
 }}
