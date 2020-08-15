48f289eefe452a7a83c30f71a1dd84dbc87ff3c7
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 11:52:11 +0200

    Create utility.js

---
+++ commands/info/utility.js
@@ -0,0 +1,19 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "help-utility",
+   category: "info",
+   description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+   let embed = new discord.MessageEmbed()
+    .setTitle("Commands: Utility")
+    .setDescription(` Prefix,announce,whois,dm,giveaway,poll,suggest,snipe,oldest,ping,findalt,report,emoji,timer,invite,slowmode,embedify,membercount.`)
+    .setColor("ORANGE")
+    .setFooter(`Made by Korabi20#8725 and owned by glitch!#3950`);
+    
+   
+    message.channel.send(`Help sent! :white_check_mark:`)
+    message.author.send(embed)
+}}
