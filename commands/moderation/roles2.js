dd76273f5e3d682490907ee867afbc4db4ecebb1
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 23:40:18 +0200

    Update roles2.js

--- commands/moderation/roles2.js
+++ commands/moderation/roles2.js
@@ -1,31 +1,56 @@
-const Discord = require("discord.js");
+const Discord  = require("discord.js");
 
 module.exports = {
-  name: "role-add",
-  aliases: ["roleadd"],
-  description: "Mute anyone who break rules",
-  category: "moderation",
-  usage: "mute <@mention> <reason>",
-  run: async (client, message, args) => {
+    name: "role-add",
+    aliases: ["roleadd"],
+    category: "moderation",
+    run: async (client, message, args) => {
+      if (!message.member.hasPermission("BAN_MEMBERS")) {
+        return message.channel.send(
+          "Sorry but you do not have permission to take roles!"
+        );
+      }
+  
+      if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send("I do not have permission to manage roles.");
+      }
+  
+      const user = message.mentions.members.first();
+  
+    if(!user) {
+      let embed8 = new Discord.MessageEmbed()
+      .setTitle("Command: r!role-add")
+      .setDescription(
+      `Description: Remove a role from a user
+      Usage: r!role-remove [user] [Role]
+      Example: r!roleadd @SaladHenry#7267 Admin`
+      )
+      .setColor("RANDOM")
+      .setFooter(`Made by Korabi20`);
+     return  message.channel.send(embed8)
+       }
+      
+      
+     let reason = args.slice(1).join(" ")
+      
+      let muterole = message.guild.roles.cache.find(x => x.name === `${reason}`)
+      
+      
+   if(user.roles.cache.has(muterole)) {
+        return message.channel.send(`Given User does not have the role "**${reason}**" `)
+      }
 
-   if (!message.member.hasPermission("BAN_MEMBERS")) {
-  return message.channel.send(
-    "Sorry but you do not have permission to take roles!"
-  );
-}
-
-if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-  return message.channel.send("I do not have permission to manage roles.");
-}
-
-const user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-const role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
-
-user.addRole(role)
-
-message.channel.send(`I have added the role ${role} to ${user}`) 
-    
- 
-    
-  }
-};
+      
+      user.roles.add(muterole)
+      
+      
+      
+      await message.channel.send(`You gave the "**${reason}**" role to ${user}`)
+      
+      user.send(`The role "**${reason}**" was given to you in **${message.guild.name}**`)
+        
+        user.roles.remove(muterole)
+      
+  
+    }
+  };
