dbdbcc87623e6180f2adaf7491649a0fcf6bcdd8
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 11:56:26 +0200

    Create infocommand.js

---
+++ commands/info/infocommand.js
@@ -0,0 +1,19 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "help-info",
+ category: "info",
+   description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+   let embed = new discord.MessageEmbed()
+    .setTitle("Commands: Info")
+    .setDescription(` Prefix,snipe,oldest,ping,findalt,emoji,support,invite,membercount.`)
+    .setColor("#ff2050")
+    .setFooter(`Made by Korabi20#8725 and owned by glitch!#3950`);
+    
+   
+    message.channel.send(`Info Commands Sent! :white_check_mark:`)
+    message.author.send(embed)
+}}
