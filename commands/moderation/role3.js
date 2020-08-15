046b9ca51e9b51a8bbc172c0f4c827f68b0b875a
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 23:12:05 +0200

    Update role3.js

--- commands/moderation/role3.js
+++ commands/moderation/role3.js
@@ -5,53 +5,23 @@ module.exports = {
     aliases: ["roleremove"],
     category: "moderation",
     run: async (client, message, args) => {
-      if (!message.member.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(
-          "Sorry but you do not have permission to take roles!"
-        );
-      }
-  
-      if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-        return message.channel.send("I do not have permission to manage roles.");
-      }
-  
-      const user = message.mentions.members.first();
-  
-    if(!user) {
-      let embed8 = new Discord.MessageEmbed()
-      .setTitle("Command: r!role-remove")
-      .setDescription(
-      `Description: Remove a role from a user
-      Usage: r!role-remove [user] [Role]
-      Example: r!rolremove @SaladHenry#7267 Admin`
-      )
-      .setColor("RANDOM")
-      .setFooter(`Made by Korabi20`);
-     return  message.channel.send(embed8)
-       }
       
-      
-     let reason = args.slice(1).join(" ")
-      
-let roles = args.join(" ").slice(22)
-const muterole = message.guild.roles.find(`name`, role)
-      
-      
-   if(user.roles.cache.has(muterole)) {
-        return message.channel.send(`Given User does not have the role "**${reason}**" `)
-      }
+     if (!message.member.hasPermission("BAN_MEMBERS")) {
+  return message.channel.send(
+    "Sorry but you do not have permission to take roles!"
+  );
+}
 
-      
-      user.roles.remove(muterole)
-      
-      
-      
-      await message.channel.send(`You took the "**${reason}**" role from the member ${user}`)
-      
-      user.send(`The role "**${reason}**" was taken from you in **${message.guild.name}**`)
-        
-        user.roles.remove(muterole)
-      
-  
+if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+  return message.channel.send("I do not have permission to manage roles.");
+}
+
+let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+let role = args.join(" ").slice(22);
+let therole = message.guild.roles.find(`name`, role);
+
+if(user.roles.has(therole.id));
+await(user.removeRole(therole.id));
+
     }
   };
