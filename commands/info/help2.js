1e0bd75013f67ea777a81afc8234d74c1a266833
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-15 12:07:42 +0200

    Create help2.js

---
+++ commands/info/help2.js
@@ -0,0 +1,26 @@
+  
+const discord = require("discord.js");
+
+module.exports = {
+  name: "help",
+  
+   category: "info",
+   description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+ let Embed = new MessageEmbed()
+        .setTitle(`Help`)
+        .setDescription(
+          `Hey \`${message.author.tag}\` Here are the help commands `
+        )
+        .setColor(`RED`)
+        .setThumbnail(Avatar)
+        .addFields(
+          { name: "r!help-moderation", value: `Sends you the moderation commands!`, inline: true },
+          { name: "r!help-info", value: `Sends you the information commands!`, inline: true },
+          { name: "r!help-fun", value: `Sends you the fun commands!`, inline: true },
+          { name: "r!help-utility", value: `Sends you the utility commands!`, inline: true },
+       );
+      
+      channel.send(Embed);
+}}
