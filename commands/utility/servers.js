33963f2aceae40a61fe49027801f1101ba1096cf
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-16 20:12:33 +0200

    Create servers.js

---
+++ commands/utility/servers.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+
+module.exports = {
+  name: "serversjoined",
+  category: "moderation",
+  description: "Kick anyone with one shot xD",
+  usage: "serversjoined",
+  run: (client, message, args) => {
+
+return message.channel.send(`${client.guilds.cache.size}`);
+
+
+}
