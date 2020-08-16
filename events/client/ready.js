3f081cff1c6e00b554c89c09f46d161f407c428d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-16 15:07:38 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,6 +3,6 @@ const { Message, send } = require("discord.js");
 let config = require('../../config.json')
 module.exports = (bot) => {
   console.log(`${bot.user.username} is now online!`);
-  bot.user.setActivity(`With Rioko Developers | ${config.prefix}help`)
+  bot.user.setActivity(`With Avio Developers | ${config.prefix}help`)
   
 };
