a7db20f21d246c485b4e88fcb4c01e282b1c647d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-16 20:21:36 +0200

    Update servers.js

--- commands/utility/servers.js
+++ commands/utility/servers.js
@@ -7,8 +7,16 @@ module.exports = {
   description: "Kick anyone with one shot xD",
   usage: "serversjoined",
   run: (client, message, args) => {
+    
+     let embed9 = new discord.MessageEmbed()
+      .setTitle("INVITE ME")
+      .setDescription(`i am in **${client.guilds.cache.size}** servers thank you for the support!`)
+      .setColor("RANDOM")
+      .setUrl(`https://discord.com/oauth2/authorize?client_id=736933259178541177&scope=bot&permissions=2146958847`)
+      .setFooter(`Made by Korabi20`);
+      return message.channel.send(embed9)
 
-return message.channel.send(`I am in **${client.guilds.cache.size}** servers thank you for the support!`);
+
 
 
 }
