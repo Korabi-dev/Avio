4c36fd3bb0243ff7690a3a6d5ab1dbb5786789a1
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-17 20:24:15 +0200

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -2,12 +2,14 @@ const discord = require("discord.js");
 
 module.exports = {
   name: "invite",
+  aliases: ["servers", "servernumber", "guildsjoined", "guilds"],
   category: "info",
   description: "invite link",
   usage: "invite",
   run: (client, message, args) => {
 let embed9 = new discord.MessageEmbed()
       .setTitle("Invite Me")
+      .setDescription(`i am in **${client.guilds.cache.size}** servers thank you for the support!`)
       .setURL(`https://discord.com/oauth2/authorize?client_id=736933259178541177&scope=bot&permissions=2146958847`)
       .setColor("RANDOM")
       .setFooter(`Thank you for inviting!`);
