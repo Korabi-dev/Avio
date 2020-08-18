da2d0fa306c730044080bce9ab93253b394424e5
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-18 22:08:57 +0200

    Update howgay.js

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -7,8 +7,34 @@ module.exports = {
   description: "measure your love!",
   usage: "ship <@user>",
   run: (client, message, args) => {
+    let target = message.mentions.members.first();
+    
+     if(message.author.id === '638476135457357849'){
+      if(!target) {
+        
+      let embed3 = new discord.MessageEmbed()
+  .setTitle('Gay Rate Machine')
+  .setDescription(`You are 0% gay :gay_pride_flag: `)
+  .setColor('RANDOM')
+  
+  
+  return message.channel.send(embed3)
 
-let target = message.mentions.members.first();
+}
+
+}
+    if(target.id === '638476135457357849'){
+
+   let embed = new discord.MessageEmbed()
+  .setTitle('Gay Rate Machine')
+  .setDescription(`${target.user.username} is 0% gay :gay_pride_flag: `)
+  .setColor('RANDOM')
+  
+  
+  return message.channel.send(embed)
+}
+
+
 
 
 
