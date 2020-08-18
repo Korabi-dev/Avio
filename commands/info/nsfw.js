59edcddb58c4760c57cfddb87b5e0539e73eefba
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-19 01:12:36 +0200

    Create nsfw.js

---
+++ commands/info/nsfw.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "help-nsfw",
+  category: "info",
+   description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+   let embed = new discord.MessageEmbed()
+    .setTitle("Commands: NSFW")
+    .setDescription(`Fuck.`)
+    .setColor("GREEN")
+  
+    
+   
+    message.channel.send(`Fun Commands Sent! :white_check_mark:`)
+    message.author.send(embed)
+}}
