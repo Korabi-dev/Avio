b4e33de0ea7209fba0e99376b94a7fbb7bc025da
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-18 03:01:25 +0200

    Create serverinfo.js

---
+++ commands/info/serverinfo.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js')
+module.exports = {
+  name: "serverinfo",
+  description: "DM a user in the guild",
+  category: "fun",
+  run: async (bot, message, args) => {
+
+    let embed9 = new discord.MessageEmbed()
+      .setTitle("Server Info")
+      .setDescription(`
+      Name: ${message.guild.name}
+      Owner: ${message.guild.owner} (`id: ${message.guild.ownerID}`)
+      Membercount: ${message.guild.membercount}
+      Region: ${message.guild.region}
+       `)
+      .setThumbnail(`${message.guild.iconURL}`)
+      .setColor("RANDOM")
+      .setFooter(`Info for ${message.guild.name}`);
+      message.channel.send(embed9)
+       
+       
+       }
+       }
