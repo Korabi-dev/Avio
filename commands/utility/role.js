2c980a40217adfaed5e0b575ec442f6a9a3d708e
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-18 14:38:03 +0200

    Update role.js

--- commands/utility/role.js
+++ commands/utility/role.js
@@ -4,7 +4,14 @@ module.exports = {
   description: "A role utility command",
   category: "utility",
   run: async (bot, message, args) => {
-message.channel.send(`Please say r!roles-remove or r!roles-add!`)
+     if (!message.member.hasPermission("BAN_MEMBERS")) {
+        return message.channel.send(
+          "Sorry but you do not have permission to use this command!"
+        );
+      }
+    
+    
+message.channel.send(`Please say r!role-remove or r!roles-add!`)
 
   }
 };
