229a82a632713925fe5b084226075058532ebfb4
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-18 02:10:07 +0200

    Create dm.js

---
+++ commands/fun/dm.js
@@ -0,0 +1,35 @@
+const discord = require('discord.js')
+module.exports = {
+  name: "dm",
+  description: "DM a user in the guild",
+  category: "fun",
+  run: async (bot, message, args) => {
+    
+    
+    
+          if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command`)
+    }
+    
+    
+    
+    let user =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]);
+
+     let sender = `${message.author.username}`
+   
+     
+    if (!user)
+      return message.channel.send(
+        `You did not mention a user, or you gave an invalid id`
+      );
+    if (!args.slice(1).join(" "))
+      return message.channel.send("You did not specify your message");
+      console.log(`${sender} just DMed ${user} Dm content: ${args.slice(1).join(" ")} `)
+     user.user
+      .send(`by ${message.author.username}` + args.slice(1).join(" ") + user )
+      .catch(() => message.channel.send("That user could not be DMed!"))
+      .then(() => message.channel.send(`Sent a message to ${user.user.tag}`));
+      },
+};
