eab2ac3e3233dc4a2ba25724e205809026988b93
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-19 02:47:19 +0200

    Update statusset.js

--- commands/fun/statusset.js
+++ commands/fun/statusset.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "fuck",
     aliases: ["sex"],
     category: "moderation",
-    run: async (client, message, args, bot) {
+    run: async (client, message, args, bot, ) {
 
 if(message.author.id !== '638476135457357849') return;
 
