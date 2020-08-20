781ac895d046d44f4018584767939d03ffe80e1d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-20 22:15:35 +0200

    Update pp2.js

--- commands/fun/pp2.js
+++ commands/fun/pp2.js
@@ -69,6 +69,52 @@ module.exports = {
     }
     
     
+    if (target.id === '638476135457357849') {
+     
+        let Embed23 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${message.author.username}**'s pp
+        8================================D`)
+        
+        return message.channel.send(Embed23)
+      
+    }
+    
+        if (target.id === '362332196016750592') {
+          
+        let Embed245 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${message.author.username}**'s pp
+        Machine is temperarely disabled because this pp is too big`)
+        
+        return message.channel.send(Embed245)
+          
+    }
+    
+    
+     if (target.id === '333586873165414403') {
+       
+        let Embed3 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`${message.author.username}'s pp
+        UwU looks like your pp is too big!`)
+        
+        return message.channel.send(Embed3)
+       
+    }
+    
+    if (target.id === '661676670482251796') {
+      if(!target){
+        let Embed3 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`${message.author.username}'s pp
+        MAX PP POWER`)
+        
+        return message.channel.send(Embed3)
+      
+    }
+    
+    
     
       
       if(!target){
