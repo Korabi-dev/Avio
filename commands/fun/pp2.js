135935eed1491373cba27c12d979faf36287403f
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-20 22:08:20 +0200

    Create pp2.js

---
+++ commands/fun/pp2.js
@@ -0,0 +1,53 @@
+const { MessageEmbed, message } = require("discord.js");
+
+module.exports = {
+  name: "ppmeter",
+  description: "There is a big chance I insult you!",
+  category: "fun",
+  run: async (bot, message, args) => {
+     let responses = [
+        "8D",
+        "8=D",
+        "8==D",
+        "8===D",
+        "8====D",
+        "8=====D",
+        "8======D",
+        "8=======D",
+        "8========D",
+        "8=========D",
+        "8==========D"
+      ];
+      
+    let target =   message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]);
+      
+      if(!target){
+  let response =
+        responses[Math.floor(Math.random() * responses.length - 1)];
+      let Embed2 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${message.author.username}**'s pp
+        ${response} 
+        `)
+        
+        .setColor(`RANDOM`);
+      message.channel.send(Embed2);
+
+
+
+
+}
+      
+    
+      let response =
+        responses[Math.floor(Math.random() * responses.length - 1)];
+      let Embed = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${target.username}**'s pp
+        ${response} 
+        `)
+        
+        .setColor(`RANDOM`);
+      message.channel.send(Embed);
+}}
