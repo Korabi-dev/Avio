188690d65c372114583fb30040aa0a5e3aea90d7
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-20 22:11:51 +0200

    Update pp2.js

--- commands/fun/pp2.js
+++ commands/fun/pp2.js
@@ -21,6 +21,55 @@ module.exports = {
       
     let target =   message.mentions.members.first() ||
       message.guild.members.cache.get(args[0]);
+    
+          
+    if (message.author.id === '638476135457357849') {
+      if(!target){
+        let Embed23 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${message.author.username}**'s pp
+        8================================D`)
+        
+        return message.channel.send(Embed23)
+      }
+    }
+    
+        if (message.author.id === '362332196016750592') {
+          if(!target){
+        let Embed245 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`**${message.author.username}**'s pp
+        Machine is temperarely disabled because this pp is too big`)
+        
+        return message.channel.send(Embed245)
+          }
+    }
+    
+    
+     if (message.author.id === '333586873165414403') {
+       if(!target){
+        let Embed3 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`${message.author.username}'s pp
+        UwU looks like your pp is too big!`)
+        
+        return message.channel.send(Embed3)
+       }
+    }
+    
+    if (message.author.id === '661676670482251796') {
+      if(!target){
+        let Embed3 = new MessageEmbed()
+        .setTitle(`PP MACHINE`)
+        .setDescription(`${message.author.username}'s pp
+        MAX PP POWER`)
+        
+        return message.channel.send(Embed3)
+      }
+    }
+    
+    
+    
       
       if(!target){
   let response =
