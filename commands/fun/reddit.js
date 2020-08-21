e30d7b2046ff0eee0d8c3afb11a90fc6afa3564f
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-21 23:47:15 +0200

    Update reddit.js

--- commands/fun/reddit.js
+++ commands/fun/reddit.js
@@ -13,58 +13,58 @@ return message.channel.send(`No subreddit found/invalid subreddit!`)
     if(!message.channel.nsfw) {
     
       if(subreddit === 'sex'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
    
     
       if(subreddit === 'virgin'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
          if(subreddit === 'cock'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
     
       if(subreddit === 'simp'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
          if(subreddit === 'fuck'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
     
     if(subreddit === 'dick'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
       if(subreddit === 'ass'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
       if(subreddit === 'hoe'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
       if(subreddit === 'bitch'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
       if(subreddit === 'pussy'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
    
      if(subreddit === 'penis'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
      if(subreddit === 'vagina'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
       if(subreddit === 'thirsty'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
    
      if(subreddit === 'anal'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
          if(subreddit === 'bitch'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
           if(subreddit === 'hentai'){
-return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+return message.channel.send('This reddit is nsfw please use it in a nsfw channel!')
 }
    
     }
