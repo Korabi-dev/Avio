618551c036621d59a332b63239f386ead7a40585
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-21 20:34:12 +0200

    Update reddit.js

--- commands/fun/reddit.js
+++ commands/fun/reddit.js
@@ -10,6 +10,40 @@ module.exports = {
 return message.channel.send(`No subreddit found!`)
 }
     
+      if(subreddit === 'simp'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+   
+    
+    if(subreddit === 'dick'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+      if(subreddit === 'ass'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+      if(subreddit === 'hoe'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+      if(subreddit === 'bitch'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+      if(subreddit === 'pussy'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+   
+     if(subreddit === 'penis'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+     if(subreddit === 'vagina'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+      if(subreddit === 'thirsty'){
+return message.channel.send('This subreddit is blacklisted due to it being nsfw!')
+}
+   
+   
+   
+   
     let img = await api(subreddit, true);
     const Embed = new MessageEmbed()
       .setTitle(`An image from r/${subreddit}`)
