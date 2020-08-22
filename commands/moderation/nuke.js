96dc1320bbf969eb88a837872b1cdc14c3957642
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-22 19:50:15 +0200

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -15,34 +15,15 @@ module.exports = {
       return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
     }
 
-   message.channel.send(`${message.author.username} are you sure you want to nuke ${message.channel.name} this cannot be undone!`)
-    
-    
-     message.channel.awaitMessages(async => m.author.id == message.author.id,
-                            {max: 1, time: 30000}).then(collected => {
-                                   if (collected.first().content.toLowerCase() == 'yes') {
-                                            message.reply("Nuking!")
-                                            let embed9 = new discord.MessageEmbed()
-                                           .setTitle("NUKE")
-                                           .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
-                                            .setColor("RANDOM")
-                                             .setImage("https://media.tenor.com/images/758fc78b8f17c82e8aeb2aa7ef4ca592/tenor.gif")
+   let embed9 = new discord.MessageEmbed()
+   .setTitle("NUKE")
+   .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
+   .setColor("RANDOM")
+                                             
      
-                                          const newchannel = await message.channel.clone()
-                                          message.channel.delete()
-                                          newchannel.send(embed9)
-                                    }
-
-                                    else
-                                             message.channel.send('Command canceled!')
-                                       
-                                    
-                                  
-                                       
-                            }).catch(() => {
-                                    message.reply('No answer after 30 seconds, operation canceled.');
-                            });
-
+    const newchannel = message.channel.clone()
+    message.channel.delete()
+    newchannel.send(embed9)
 
 }
 }
