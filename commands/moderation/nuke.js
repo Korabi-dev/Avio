b3de33b0683c7ea2706148540d31acfdc361ce75
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-22 18:44:08 +0200

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -5,6 +5,15 @@ module.exports = {
   description: "Get a meme!",
   category: "fun",
   run: async (bot, message, args) => {
+    
+    
+      if(!message.member.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command`)
+    }
+    
+    if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
+    }
 
    let embed9 = new discord.MessageEmbed()
       .setTitle("NUKE")
