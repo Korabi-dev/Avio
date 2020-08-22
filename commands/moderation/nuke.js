8dc678a914ae4fc7a9a6d6174dd9c75be7d253bc
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-22 19:35:55 +0200

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -15,15 +15,35 @@ module.exports = {
       return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
     }
 
-   let embed9 = new discord.MessageEmbed()
-      .setTitle("NUKE")
-      .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
-      .setColor("RANDOM")
-     .setImage("https://media.tenor.com/images/758fc78b8f17c82e8aeb2aa7ef4ca592/tenor.gif")
+   
+    
+    
+     message.channel.awaitMessages(m => m.author.id == message.author.id,
+                            {max: 1, time: 30000}).then(collected => {
+                                   if (collected.first().content.toLowerCase() == 'yes') {
+                                            message.reply("Nuking!")
+                                            let embed9 = new discord.MessageEmbed()
+                                           .setTitle("NUKE")
+                                           .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
+                                            .setColor("RANDOM")
+                                             .setImage("https://media.tenor.com/images/758fc78b8f17c82e8aeb2aa7ef4ca592/tenor.gif")
      
-     const newchannel = await message.channel.clone()
-      message.channel.delete()
-    newchannel.send(embed9)
+                                          const newchannel = await message.channel.clone()
+                                          message.channel.delete()
+                                          newchannel.send(embed9)
+                                    }
+
+                                    else
+                                             message.channel.awaitMessages(m => m.author.id == message.author.id,
+                            {max: 1, time: 30000}).then(collected => {
+                                   if (collected.first().content.toLowerCase() == 'no'{
+                                       message.channel.send('Command Caneled')
+                                    }
+                                  
+                                       
+                            }).catch(() => {
+                                    message.reply('No answer after 30 seconds, operation canceled.');
+                            });
 
 
 }
