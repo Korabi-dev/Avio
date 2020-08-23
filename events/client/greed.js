f63c6079f67a14fbcb1ee2e0d6a25fc53256a8d1
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-23 16:46:52 +0200

    Update greed.js

--- events/client/greed.js
+++ events/client/greed.js
@@ -5,7 +5,9 @@ module.exports = (bot) => {
 
 bot.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.find(ch => ch.name == 'welcome');
-if (!channel) return;
+if (!channel){
+console.log('No channel found!')
+}
   channel.send(`Welcome to the server, ${member}!`);
 });
 };
