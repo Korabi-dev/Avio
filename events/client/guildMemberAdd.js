262808693e15ce7967c15bf63e8b9820a9d62b44
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-23 17:09:07 +0200

    Delete guildMemberAdd.js

--- events/client/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-const { Message, send } = require("discord.js");
-
-let config = require('../../config.json')
-module.exports = (bot, member) => {
-  member.user.send(`Hey ${member.user.tag} welcome to ${member.guild.name} have fun!`)
-}
