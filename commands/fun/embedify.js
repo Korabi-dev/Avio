e76fe6b980a24fb2abc56bd17e310ada719745b7
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-24 19:35:13 +0200

    Update embedify.js

--- commands/fun/embedify.js
+++ commands/fun/embedify.js
@@ -4,13 +4,25 @@ module.exports = {
     description: "Get the bot to say what ever you want! embeded",
     usage: "<msg>",
     run: async (bot, message, args) => {
-      if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
-      let MSG = message.content.split(`${bot.prefix}embedify `).join("");
-      if (!MSG)
-        return message.channel.send(`You did not specify your message to send!`);
+      
+      if(!args[0]){
+return message.channel.send(`Oops,You forgot to mention a title for the embed!`)
+}
+        
+       if(!args[1]) {
+return message.channel.send(`Oops,You forgot to mention a description for the embed!`)
+}
+        
+      if(!args[2]){
+          return message.channel.send(`Oops,You forgot to mention a footer for the embed!`)
+}  
+        
+        
     let embed = new discord.MessageEmbed()
-    .setTitle(MSG)
-    .setColor("#ff2050")
+    .setTitle(args[0])
+    .setDescription(args[1])
+    .setFooter(args[2])
+    .setColor("RANDOM")
    
     message.delete();
     message.channel.send(embed)
