5afc65c5ce3f06350de3594c3ca07391d43ea690
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-26 04:31:36 +0200

    Update av.js

--- commands/fun/av.js
+++ commands/fun/av.js
@@ -11,7 +11,7 @@ module.exports = {
 let Embed2 = new MessageEmbed()
         .setTitle(`AVATAR`)
         .setDescription(`${message.author.username}'s avatar`)
-        .setImage(message.author.displayAvatarURL({ dynamic: true, size: 256 }))
+        .setImage(message.author.displayAvatarURL({ dynamic: true, size: 2056 }))
         .setColor(`RANDOM`);
       message.channel.send(Embed2);
 }
