87a15f9cd2acad8b645a7557fa78df8b2b6ece61
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-26 15:30:52 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,7 +3,10 @@ const { Message, send } = require("discord.js");
 let config = require('../../config.json')
 module.exports = (bot) => {
   console.log(`${bot.user.username} is now online!`);
-  bot.user.setActivity(`With Avio Developers | ${config.prefix}help`)
-  bot.user.setStatus("dnd");
+ 
+  
+  bot.user.setPresence({ activity: { name: `With Avio Developers | ${config.prefix}help` }, status: 'dnd' })
+  
+
   
 };
