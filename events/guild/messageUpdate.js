c8571d6b88b67a7e3f71becbe3df46ea6e769118
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-28 00:34:42 +0200

    Update messageUpdate.js

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -12,7 +12,9 @@ module.exports = async (oldMessage, newMessage) => {
     let channel = oldMessage.guild.channels.cache.find(
       (ch) => ch.name === "edit-log"
     );
-    if (!channel) return;
+    if (!channel) {
+return oldMessage.channel.send(`Hey i've just noticed ${oldMessage.author.username} edited a message to view the old content you need to make a channel called \`edit-log\` so i can log the message  there next time!`)
+}
     channel.send(embed);
   } catch (e) {}
 };
