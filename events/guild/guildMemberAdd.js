1f22969a6527da123fd9495848cb6984578ca40d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-29 00:55:34 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -3,5 +3,5 @@ const { Message, send } = require("discord.js");
 
 let config = require('../../config.json')
 module.exports = (bot, member) => {
-  console.log('e')
+  member.send(`Hey ${member} welcome to ${member.guild.name} have fun!`)
 }
