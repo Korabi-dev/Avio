71b4f597a7b4b688574003a774f56bb91c6d0b77
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 00:40:50 +0200

    Delete announce.js

--- commands/fun/announce.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-module.exports = {
-  name: "announce",
-  description: "Get the bot to say what ever you want in a specific channel.",
-  usage: "<channel id> <msg>",
-  run: async (bot, message, args) => {
-    
- message.channel.send(`Command is temperarely disabled.`)
-};
