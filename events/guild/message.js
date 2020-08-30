32d0993a3f3b8b86f5fa2f6bf8776e1aa610dc32
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 20:02:38 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -155,13 +155,15 @@ module.exports = async (bot, message) => {
      return message.channel.send(Embed);
   }
   
-    if(command.author.id === '356324345783648257') {
-return message.channel.send(`You have been blacklisted from this bot reason : abusing it and rude to the dev!`)
-}
+
 
 
   let command = bot.commands.get(cmd);
   if (!command) command = bot.commands.get(bot.aliases.get(cmd));
+  
+      if(command.author.id === '356324345783648257') {
+return message.channel.send(`You have been blacklisted from this bot reason : abusing it and rude to the dev!`)
+}
 
   if (command) {
     if (command.timeout) {
