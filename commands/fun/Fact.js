27e33d753f9f3de371cff62ddd6a3e35e12f8400
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 04:17:10 +0200

    Create Fact.js

---
+++ commands/fun/Fact.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const factful = require('factful.js')
+
+module.exports = {
+    name: "fact",
+    aliases: ["randomfact"],
+    description: "Get a random fact",
+    usage:"fact",
+    category:"fun",
+    aliases: null,
+    run: async (client,message,args) =>{
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Random Fact')
+        .setDescription(`${factful.fact().all}`)
+        .setColor('RANDOM')
+        message.channel.send(embed)
+    }
+}
