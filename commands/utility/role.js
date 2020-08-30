e8fa2c6f2fe5a00bdec34e48149a8073da8c2665
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 21:14:49 +0200

    Update role.js

--- commands/utility/role.js
+++ commands/utility/role.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'grole',
+    name: 'role',
     run: async (client, message, args) => {
 
         message.delete();
