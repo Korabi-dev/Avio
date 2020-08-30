dab93b52db512e12c9780b90254f7608dc7e5909
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-31 00:09:26 +0200

    Update refresh.js

--- commands/fun/refresh.js
+++ commands/fun/refresh.js
@@ -1,22 +1,13 @@
-  
-  
-const Discord = require("discord.js");
-
-
-
 module.exports = {
-  name: "refresh",
-  description: "There is a big chance I insult you!",
-  category: "fun",
-  run: async (bot, message, args) => {
-  
- if (message.author.id !== '638476135457357849')
+    name: "restart",
+  aliases: ["refresh"],
+    category: "owner",
+    run: async (client, message, args) => {
+         if (message.author.id !== '638476135457357849')
       return message.channel.send(
-        `You do not have premissions to use this command, ${message.author.username} you are not a dev!`
+        `You do not have premissions to use this command, ${message.author.username} you are not a developer!`
       );
-      
-      message.channel.send('refreshing wait 10 seconds please!')
-   
-  
-  }
-  }
+        await message.channel.send(`Restarting bot...`)
+        process.exit();
+    }
+}
