ddc02c5d9463ce1ff0c95b063de00ea182b879b6
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 05:21:46 +0200

    Update avatar.js

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -1,48 +1,78 @@
-const { MessageEmbed } = require("discord.js");
-const { formatDate } = require("../../functions");
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
-  name: "whois",
-  description: "Get your own or someone else's avatar",
-  usage: "[user mention]",
-  category: "fun",
-  run: async (bot, message, args) => {
-    let Embed = new MessageEmbed();
-    let roles = [];
-    if (!message.mentions.users.first()) {
-      message.member.roles.cache.forEach((role) => {
-        roles.push(role);
-      });
-      Embed.setTitle(`Your information!`);
-      Embed.setThumbnail(message.author.displayAvatarURL());
-      Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `**JoinDate:** ${formatDate(message.member.joinedAt)}  **Created Date:**   ${message.author.createdAt}
-          **\nID:**
-           ${
-          `${message.author.id}`
-        } 
-          **\nRoles:**
-        ${roles}`
-      );
-      return message.channel.send(Embed);
-    } else {
-      let User = message.mentions.members.first();
-      User.roles.cache.forEach((role) => {
-        roles.push(role);
-      });
-      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s info!`);
-      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
-      Embed.setColor(`RANDOM`);
-      Embed.setDescription(
-        `**Joined:** ${formatDate(User.joinedAt)} **Created Date:**  ${User.user.createdAt}
-         **\nID:**
-           ${
-          `${User.id}`
-        } 
-         **\nRoles:** 
-        ${roles}`
-      );
-      return message.channel.send(Embed);
+    name: "user-info",
+    aliases: ["whois"],
+    category: "extra",
+    run: async (client, message, args) => {
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+        let status;
+        switch (user.presence.status) {
+            case "online":
+                status = "<:online:729181184193462285> online";
+                break;
+            case "dnd":
+                status = "<:dnd:729181212530442311> dnd";
+                break;
+            case "idle":
+                status = "<:idle:729181121933475931> idle";
+                break;
+            case "offline":
+                status = "<:offline:729181162182017051> offline";
+                break;
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`${user.user.username} stats`)
+            .setColor(`#f3f3f3`)
+            .setThumbnail(user.user.displayAvatarURL({dynamic : true}))
+            .addFields(
+                {
+                    name: "Name: ",
+                    value: user.user.username,
+                    inline: true
+                },
+                {
+                    name: "#️⃣ Discriminator: ",
+                    value: `#${user.user.discriminator}`,
+                    inline: true
+                },
+                {
+                    name: "🆔 ID: ",
+                    value: user.user.id,
+                },
+                {
+                    name: "Current Status: ",
+                    value: status,
+                    inline: true
+                },
+                {
+                    name: "Activity: ",
+                    value: user.presence.activities[0] ? user.presence.activities[0].name : `User isn't playing a game!`,
+                    inline: true
+                },
+                {
+                    name: 'Avatar link: ',
+                    value: `[Click Here](${user.user.displayAvatarURL()})`
+                },
+                {
+                    name: 'Creation Date: ',
+                    value: user.user.createdAt.toLocaleDateString("en-us"),
+                    inline: true
+                },
+                {
+                    name: 'Joined Date: ',
+                    value: user.joinedAt.toLocaleDateString("en-us"),
+                    inline: true
+                },
+                {
+                    name: 'User Roles: ',
+                    value: user.roles.cache.map(role => role.toString()).join(" ,"),
+                    inline: true
+                }
+            )
+
+        await message.channel.send(embed)
     }
-  },
-};
+}
