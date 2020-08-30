e75233cb7aa08cf5ffaf8d8a3adf9a8723198188
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 06:05:20 +0200

    Create nickname.js

---
+++ commands/moderation/nickname.js
@@ -0,0 +1,36 @@
+  
+const { MessageEmbed, discord } = require('discord.js');
+
+module.exports = {
+    name: "nickname",
+    aliases: ["nick"],
+    category: "extra",
+    run: async (client, message, args) => {
+    
+    
+    
+    let target =   message.mentions.members.first(); 
+    
+    let nickname = args.slice(0).join(" ")
+    
+        if(!message.member.hasPermission("MANAGE_NICKNAMES")) {
+      return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command`)
+    }
+    
+    if(!message.guild.me.hasPermission("MANAGE_NICKNAMES")) {
+      return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
+    }
+    
+    if(!target){
+return message.channel.send(`Please mention someone!`)
+}
+    
+    if(!args[0]){
+return message.channel.send('please mention a new nickname for the user!')
+}
+ 
+ target.setNickname(nickname)
+        
+
+    }
+}
