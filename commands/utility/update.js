71f932e347edd535566dfe382d072b8760c146d3
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 19:19:07 +0200

    Create update.js

---
+++ commands/utility/update.js
@@ -0,0 +1,45 @@
+const discord = require("discord.js")
+const { OwnerID } = require('../../config.json')
+ 
+module.exports = {
+  name: "update",
+  category: "info",
+  description: "Get the stats of corona",
+  usage: "corona all or corona <country>",
+  run: async (client, message, args, guild ) => {
+ 
+    if(message.author.id !== OwnerID) return message.channel.send(`I am sorry ${message.author.username} but only the Owner of this bot can use this command`)
+ 
+    if (!args[0]) {
+      return message.channel.send(`${message.author.username} I need to know if its a \`Minor\` or \`Mega\` update.`)
+    }
+ 
+    if (!args.slice(1).join(" ")) {
+      return message.channel.send(`${message.author.userna} I need some nots for my upate `)
+    }
+ 
+     if(message.channel.name == 'update' && !message.author.bot){
+   client.guilds.cache.forEach(guild=>{
+     if(guild == message.guild) return;
+     let channel = guild.channels.cache.find(ch=>ch.name === '»🔗«avio-bot-updates');
+     if(!channel) return; 
+     let embed = new discord.MessageEmbed()
+     .setTitle(`${client.user.username} Updates ✉️`)
+     .addFields(
+       {name: "**Update Type**", 
+        value: `${args[0]}`,
+       inline: false }, // If you want them inline do true if you don't Want it like that just leave is false
+     { name: "**Notes**", 
+      value: args.slice(1).join(" "),
+      inline: false }
+ 
+     ) 
+     .setTimestamp()
+     .setAuthor(client.user.tag, client.user.displayAvatarURL())
+     .setFooter(message.author.tag , message.author.displayAvatarURL())
+     channel.send(embed)
+   })
+ }
+ 
+    }
+}
