c84440048c1396f5ae79685a501fd808dbf8544c
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-30 05:52:37 +0200

    Update moderation.js

--- commands/info/moderation.js
+++ commands/info/moderation.js
@@ -9,7 +9,7 @@ module.exports = {
   run: (client, message, args) => {
    let embed = new discord.MessageEmbed()
     .setTitle("Commands: Moderation")
-    .setDescription(` report,warn,mute,kick,warns,ban.`)
+    .setDescription(` report,warn,mute,kick,warns,ban,unban.`)
     .setColor("RED")
     .setFooter(`Made by Korabi20#8725 and owned by glitch!#3950`);
     
