9891e482b9dc25922d9736caa1fd280b728f53e1
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 00:55:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ client.vote = new Map();
 
   readdirSync("./commands/").map((dir) => {
     const commands = readdirSync(`./commands/${dir}/`).map((cmd) => {
-      let pull = require(`./commands/${dir}/${cmd}`);
+      let pull = require(`./commands/config/${cmd}`);
       console.log(`Loaded command ${pull.name} m!`);
       client.commands.set(pull.name, pull);
       if (pull.aliases) {
-        if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
+        if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
       }
     });
   });
