aefdcf3898d3ee9fab818e80c4ec977c47d56d88
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-31 18:41:15 +0200

    Update refresh.js

--- commands/fun/refresh.js
+++ commands/fun/refresh.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: "shutdown",
-  aliases: ["crash"],
+    name: "refresh",
+  aliases: ["restart", "reboot", "reload", "off/on", "devrefresh"],
     category: "owner",
     run: async (client, message, args) => {
          if (message.author.id !== '638476135457357849')
       return message.channel.send(
         `You do not have premissions to use this command, ${message.author.username} you are not a developer!`
       );
-        console.log(`Shutting down...`)
-        await message.channel.send(`Shutting down!`)
+        console.log(`Refreshing...`)
+        await message.channel.send(`Refreshing!`)
         process.exit();
     }
 }
