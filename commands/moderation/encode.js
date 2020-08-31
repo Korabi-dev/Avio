f4607ea91bbd07169e6032c872cb2991cc1ed61d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-08-31 05:05:19 +0200

    Create encode.js

---
+++ commands/moderation/encode.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: "encode",
+    category: "extra",
+    run: async (client, message, args) => {
+        const url = `https://some-random-api.ml/binary?text=${args.slice(0).join("%20")}`;
+
+        let response, data;
+        try {
+            response = await axios.get(url);
+            data = response.data;
+        } catch (e) {
+            return message.channel.send(`error (-1) please report this to the dev!`)
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle('Encode Binary')
+            .setDescription(data.binary)
+            .setColor("GREEN")
+            
+        await message.channel.send(embed)
+    }
+}
