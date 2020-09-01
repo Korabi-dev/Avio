0a35d9d9ccd56a061d1351073a942b39c7abb316
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 07:36:08 +0200

    Update system.js

--- commands/utility/system.js
+++ commands/utility/system.js
@@ -4,14 +4,14 @@ const { QUEUE_LIMIT, COLOR } = require("../../config.json");
 
 module.exports = {
   async play(song, message) {
-    const queue = message.bot.queue.get(message.guild.id);
+    const queue = bot.queue.get(message.guild.id);
 let embed = new MessageEmbed()
 .setColor(COLOR);
 
     if (!song) {
       queue.channel.leave();
-      message.bot.queue.delete(message.guild.id);
-      embed.setAuthor("MUSIC QUEUE IS ENDED NOW :/")
+      bot.queue.delete(message.guild.id);
+      embed.setAuthor("MUSIC QUEUE IS ENDED NOW ")
       return queue.textChannel
         .send(embed)
         .catch(console.error);
@@ -33,10 +33,20 @@ let embed = new MessageEmbed()
 
     const dispatcher = queue.connection
       .play(stream, { type: "opus" })
+     .on("finish", () => {
+        if (queue.loop) {
+          let lastsong = queue.songs.shift();
+          queue.songs.push(lastsong);
+          module.exports.play(queue.songs[0], message);
+        } else {
+          queue.songs.shift();
+          module.exports.play(queue.songs[0], message);
+        }
+      })
       .on("error", console.error);
   
     dispatcher.setVolumeLogarithmic(queue.volume / 100); //VOLUME
-embed.setAuthor("Started Playing Song", message.bot.user.displayAvatarURL())
+embed.setAuthor("Started Playing Song", bot.user.displayAvatarURL())
     .setDescription(`**[${song.title}](${song.url})**`)
     
     queue.textChannel
