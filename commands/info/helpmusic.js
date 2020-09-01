d2772ce8e662d08abc55a6722aa48c1bb889d113
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 03:00:10 +0200

    Create helpmusic.js

---
+++ commands/info/helpmusic.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "help-music",
+  category: "info",
+   description: "get the commands",
+  usage: "help",
+  run: (client, message, args) => {
+   let embed = new discord.MessageEmbed()
+    .setTitle("Commands: Music")
+    .setDescription(`Play,Stop.`)
+    .setFooter(`note: commands are still in development may be a little buggy`)
+    .setColor("GREEN")
+  
+    
+   
+    message.channel.send(`Music Commands Sent! :white_check_mark:`)
+    message.author.send(embed)
+}}
