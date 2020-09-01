9ecb0c279e54243fe18d4b52185a5e149e892929
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 11:00:46 +0200

    Update play.js

--- commands/config/play.js
+++ commands/config/play.js
@@ -15,6 +15,8 @@ module.exports = {
   name: "play",
   description: "Play the song and feel the music",
   run: async (client, message, args) => {
+    client.queue = new Map();
+    client.vote = new Map();
     let embed = new MessageEmbed()
 .setColor(COLOR);
 
