ee8287e9a456dcbc8a0c818045de06a9e2cd515d
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 22:45:21 +0200

    Create leave.js

---
+++ commands/config/leave.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js")
+const { COLOR } = require("../../config.json");
+
+
+
+const discord = require("discord.js");
+
+module.exports = {
+  name: "leave",
+  aliases: ["disconnect"],
+  description: "Stop the music and take rest ;)",
+   run: async(client, message, args) => {
+     try {
+     let embed = new MessageEmbed()
+.setColor("BLUE");
+
+    const { channel } = await message.member.voice;
+      
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      embed.setAuthor("YOU NEED TO BE IN VOICE CHANNEL")
+      return message.channel.send(embed);
+    }
+    
+
+     
+    
+     channel.leave();
+     
+     
+     
+     let left = new MessageEmbed()
+     .setColor("BLUE")
+     .setDescription("Sucessfully left the voice channel!")
+     .setFooter(`This won't work if you are in a different channel this is just a step to prevent abuse/trolling.`)
+     message.channel.send(left)
+     } catch (error) {
+            return message.channel.send(`oops an error was detected: ${error}`)
+        }
+  }
+};
