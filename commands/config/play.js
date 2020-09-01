095ae7eed8ab7ff51a9a403f49520f39cb21d9b1
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 10:03:54 +0200

    Update play.js

--- commands/config/play.js
+++ commands/config/play.js
@@ -129,7 +129,7 @@ module.exports = {
     if (!serverQueue) {
       try {
         queueConstruct.connection = await channel.join();
-        play(queueConstruct.songs[0], message);
+        play(queueConstruct.songs[1], message);
       } catch (error) {
         console.error(`Could not join voice channel: ${error}`);
         bot.queue.delete(message.guild.id);
