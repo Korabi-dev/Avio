c13852043a0b466352eeb08f3051d1cbaa565273
Korabi20-dev <69105206+Korabi20-dev@users.noreply.github.com>
2020-09-01 05:23:50 +0200

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,4 +1,6 @@
 const Timers = new Map();
+const discord = require('discord.js');
+const client = new discord.Client();
 client.queue = new Map();
  client.vote = new Map();
 module.exports = { Timers: Timers };
