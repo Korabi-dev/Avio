12bfa1f16013cf9cd11b994ff54d579bd1c69f5f
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-03 00:05:38 +0200

    Update webhook.js

--- commands/owner/webhook.js
+++ commands/owner/webhook.js
@@ -5,12 +5,14 @@ module.exports = {
     name: "hooktest",
     category: "owner",
     run: async (client, message, args) => {
+        
+
         const embed = new discord.MessageEmbed()
     .setTitle('Avio')
     .setDescription(`Wow dear dev,you are using webhooks now huh thats sad.`)
 	.setColor('#0099ff');
         try {
-            const channel = client.channels.get('723679963282276387');
+            const channel = client.channels.chache.get('723679963282276387');
             const webhooks = await channel.fetchWebhooks();
             const webhook = webhooks.first();
     
