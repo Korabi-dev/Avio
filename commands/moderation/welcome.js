aa6188bef2066ac9470df99d51155a25a972d569
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-02 02:50:56 +0200

    Create welcome.js

---
+++ commands/moderation/welcome.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: "welcome",
+    aliases: ["hello"],
+    category: "extra",
+    run: async (client, message, args) => {
+    
+        
+        
+        const user = message.mentions.members.first();
+    
+        if(!user){
+
+            return message.channel.send('Please provide a member')
+        }
+    
+        const url = 'https://api.no-api-key.com/api/v2/welcome?username=${user.username}&text_heading=Welcome to Korabi server&user_image=${user.displayAvatarURL({ format: "png" }`';
+
+        try {
+            const embed = new MessageEmbed()
+              .setImage(url)
+            await message.channel.send(embed)
+        } catch (e) {
+            return message.channel.send(`Error (${e}) please report this to the dev`)
+        }
+
+    }
+}
