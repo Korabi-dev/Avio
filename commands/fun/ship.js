8c60cebcf6845b92c9b8885fe66fbc54e010bd98
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-05 00:43:38 +0200

    eeee

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -20,7 +20,7 @@ if(message.author.id === '325558938005143562'){
   if(target.id === '707676620390924364') {
     let embed2 = new discord.MessageEmbed()
     .setTitle('LOVE')
-    .setDescription(`${message.author} is 100% in love with ${target}`)
+    .setDescription(`${message.author} is 100% in love with ${target} GET MARRIED CMON`)
     .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
     .setColor('RED')
 
@@ -32,7 +32,7 @@ if(message.author.id === '707676620390924364'){
   if(target.id === '325558938005143562') {
     let embed1 = new discord.MessageEmbed()
     .setTitle('LOVE')
-    .setDescription(`${message.author} is 100% in love with ${target}`)
+    .setDescription(`${message.author} is Simp% in love with ${target}`)
     .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
     .setColor('RED')
 
--- commands/utility/serverslist.js
+++ commands/utility/serverslist.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
 
 module.exports = {
-  name: "guildslist",
+  name: "eval-console.log(servers);",
   description: "A role utility command",
   category: "utility",
   run: async (bot, message, args) => {
