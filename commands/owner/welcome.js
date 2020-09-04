141e69a97c43cbe99e21933552c958e7d216c6ed
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-04 15:24:21 +0200

    Update welcome.js

--- commands/owner/welcome.js
+++ commands/owner/welcome.js
@@ -7,7 +7,7 @@ module.exports = {
     run: async (client, message, args) => {
         let user = message.mentions.members.first();
         
-    const url = `https://api.no-api-key.com/api/v2/welcome?username=${user.username}&text_heading=Welcome%20To%20${message.guild.name}!&user_image=${user.displayAvatarURL({ format: "png" })}`
+    const url = `https://api.no-api-key.com/api/v2/welcome?username=${user.username}&text_heading=Welcome%20To%20${message.guild.name}!&user_image=${user.user.displayAvatarURL({ dynamic: true, size: 256 })}`
         
     
         let response, data;
