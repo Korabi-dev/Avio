20ea6e632d33deed37279d100219b95182cc9b92
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-05 00:30:09 +0200

    Update ship.js

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -16,6 +16,31 @@ return message.channel.send(`**${message.author.username}**, I know you love you
 
 }
 
+if(message.author.id === '325558938005143562'){
+  if(target.id === '707676620390924364') {
+    let embed = new discord.MessageEmbed()
+    .setTitle('LOVE')
+    .setDescription(`${message.author} is 100% in love with ${target}`)
+    .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
+    .setColor('RED')
+
+
+
+  }
+}
+if(message.author.id === '707676620390924364'){
+  if(target.id === '325558938005143562') {
+    let embed = new discord.MessageEmbed()
+    .setTitle('LOVE')
+    .setDescription(`${message.author} is 100% in love with ${target}`)
+    .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
+    .setColor('RED')
+
+
+
+  }
+}
+
     
 let response = Math.floor(`${Math.random() * 100}`) + 1;
 
