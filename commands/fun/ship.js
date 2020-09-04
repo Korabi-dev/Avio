284a1ba68e5faf4b2afad655a95a2054093e8d0d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-05 00:36:33 +0200

    Update ship.js

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -18,14 +18,14 @@ return message.channel.send(`**${message.author.username}**, I know you love you
 
 if(message.author.id === '325558938005143562'){
   if(target.id === '707676620390924364') {
-    let embed = new discord.MessageEmbed()
+    let embed2 = new discord.MessageEmbed()
     .setTitle('LOVE')
     .setDescription(`${message.author} is 100% in love with ${target}`)
     .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
     .setColor('RED')
 
 
-
+    return message.channel.send(embed2)
   }
 }
 if(message.author.id === '707676620390924364'){
@@ -44,14 +44,14 @@ if(message.author.id === '707676620390924364'){
     
 let response = Math.floor(`${Math.random() * 100}`) + 1;
 
-  let embed2 = new discord.MessageEmbed()
+  let embed = new discord.MessageEmbed()
   .setTitle('LOVE')
   .setDescription(`${message.author} is ${response}% in love with ${target}`)
   .setImage('https://media1.tenor.com/images/53531b7811058448306f67e0934a6aa2/tenor.gif?itemid=3555133')
   .setColor('RED')
   
   
-  return message.channel.send(embed2)
+  return message.channel.send(embed)
   
 
 
