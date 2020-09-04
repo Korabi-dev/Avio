92f35589d7deb3778d5a85b50f98d701fd3a3ea2
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-04 23:38:03 +0200

    Create messagelogs.js

---
+++ commands/config/messagelogs.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "messagelogs-setup",
+    description: "There is a big chance I insult you!",
+    category: "fun",
+    run: async (bot, message, args) => {
+        let channel1 = message.guild.channels.cache.find(
+            (ch) => ch.name === "message-log"
+          );
+
+          if(channel1) {
+            return message.channel.send(`There is already a channel nammed "message-logs" in this server!`)
+        }
+
+        const channel = await message.guild.channels.create(message-log, {
+            type: "text",
+          topic: "Mail is Direct Opened by **" + message.author.username + "** to make contact with " + message.author.tag
+        })
+
+
+
+    }
+}
