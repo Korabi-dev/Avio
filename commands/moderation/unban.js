3f67bb4ca543a3625e6d41b285ca9c871750c8c9
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-06 04:22:56 +0200

    e

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -20,7 +20,7 @@ module.exports = {
             })
             await message.channel.send(`${member.user.username} has been unbanned!`)
         } catch (e) {
-            return message.channel.send(`An error occured!`)
+            return message.channel.send(`${member.user.username}has been unbanned!`)
         }
 
     }
