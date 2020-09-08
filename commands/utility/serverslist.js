a9b44bbf9067b488b7b983b57f3f3d7646c714a3
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-08 22:41:19 +0200

    Update serverslist.js

--- commands/utility/serverslist.js
+++ commands/utility/serverslist.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
 
 module.exports = {
-  name: "eval-console.log(servers);",
+  name: "serverslist",
   description: "A role utility command",
   category: "utility",
   run: async (bot, message, args) => {
