d9c7fcfe376b9cf5c04cfd023f008ec21e3de509
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-08 22:14:36 +0200

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -15,15 +15,53 @@ module.exports = {
       return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`)
     }
 
-   let embed9 = new discord.MessageEmbed()
-   .setTitle("NUKE")
-   .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
-   .setColor("RANDOM")
-                                             
-     
-    const newchannel = await message.channel.clone()
-    message.channel.delete()
-    newchannel.send(embed9)
+
+    try {
+      let embed90 = new discord.MessageEmbed()
+            .setTitle("NUKE")
+            .setDescription(`Hey ${message.author.username} are you sure you want to nuke this channel this will delete it and make a new one Please react with  👍 or 👎`)
+            .setColor('RANDOM')
+            const msg = await message.channel.send(embed90)
+      
+      
+      
+        msg.react('👍').then(r => {
+                                  msg.react('👎');
+                          });
+      
+                      
+                          msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.name == '👍' || reaction.emoji.name == '👎'),
+                                  { max: 1, time: 30000 }).then(async collected => {
+                                          if (collected.first().emoji.name == '👍') {
+                                            let embed9 = new discord.MessageEmbed()
+                                            .setTitle("NUKE")
+                                            .setDescription(`This channel was nuked nuked by **${message.author.username}**`)
+                                            .setColor("RANDOM")
+                                                                                      
+                                              
+                                             const newchannel = await message.channel.clone()
+                                             message.channel.delete()
+                                             newchannel.send(embed9)
+                                                  
+                                          }
+                                          else
+                                                  message.channel.send(`Command canceled!`);
+                                                  msg.delete();
+                                  }).catch(() => {
+                                          message.channel.send(`No Reaction after 30 seconds, command canceled!`)
+                                  });
+                                  
+              
+              } catch (e) {
+               const embed = new discord.MessageEmbed()
+                      .setTitle(`An Error has occured: ${e}`)
+                  return message.channel.send(embed)
+              }
+              
+
+
+
+ 
 
 }
 }
