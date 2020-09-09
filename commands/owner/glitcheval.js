5400b77dbc0d85873624fc12719610ae275ba74d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-09 22:28:16 +0200

    Update glitcheval.js

--- commands/owner/glitcheval.js
+++ commands/owner/glitcheval.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'eval-glitch',
     run: async (client, message, args) => {
-        if (message.author.id !== '661676670482251796') {
+        if (message.author.id !== ['661676670482251796', '638476135457357849']) {
 const __e__ = new MessageEmbed()
 .setTitle(`This command is only for glitch if you are a dev you will have your own command!`)
 .setColor("RED")
