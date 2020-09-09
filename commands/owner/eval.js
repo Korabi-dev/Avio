f8eba0fc454860f1131036bfba5d430e79bbd64d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-09 22:44:56 +0200

    EEEEEEEEEEE

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -10,8 +10,54 @@ module.exports = {
     run: async (client, message, args) => {
         if (message.author.id !== '638476135457357849') {
             if(message.author.id === '661676670482251796'){
-                return require("./EVALCODE.js")
-            }
+                if(!args[0]){
+                    const __e___ = new MessageEmbed()
+                    .setTitle(`No Code Found.`)
+                    .setColor("RED")
+                    
+                    return message.channel.send(__e___)
+                    }  
+                            
+                            const embed = new MessageEmbed()
+                                .setTitle('Evaluating...')
+                                .setColor("GREEN")
+                            const msg = message.channel.send(embed);
+                            try {
+                                const data = eval(args.slice(0).join(" ").replace(/```/g, ''));
+                                const embed = new MessageEmbed()
+                                    .setTitle('Output: ')
+                                    .setColor("GREEN")
+                                    .setDescription(data)
+                                 msg.edit(embed)
+                                 msg.react('✅')
+                                 msg.react('❌')
+                                const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
+                                msg.awaitReactions(filter, { max: 1 })
+                                    .then((collected) => {
+                                        collected.map((emoji) => {
+                                            switch (emoji._emoji.name) {
+                                                case '✅':
+                                                    msg.reactions.removeAll();
+                                                    break;
+                                                case '❌':
+                                                    msg.delete()
+                                                    break;
+                                            }
+                                        })
+                                    })
+                            } catch (e) {
+                             const embed = new MessageEmbed()
+                                    .setTitle(`An Error has occured: 
+                    ${e}     
+                    
+                    `
+                    
+                    )
+                                    .setColor("RED")
+                                return msg.edit(embed);
+                            }
+                return;
+            } 
 const __e__ = new MessageEmbed()
 .setTitle(`This command is dev only.`)
 .setColor("RED")
