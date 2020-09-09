9323b8d4a668ad20cc9a7f308e13d1f2fb525840
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-10 00:44:15 +0200

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -8,7 +8,7 @@ module.exports = (bot) => {
   readdirSync("./commands/").map((dir) => {
     const commands = readdirSync(`./commands/${dir}/`).map((cmd) => {
       let pull = require(`../commands/${dir}/${cmd}`);
-      console.log(`Loaded command ${pull.name} 1`);
+      console.log(`Loaded command ${pull.name}!`);
       bot.commands.set(pull.name, pull);
       if (pull.aliases) {
         if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
