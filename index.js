30cae2335642b9ab750918a28baab5c74b79ef2d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-09 21:17:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,14 +40,14 @@ If you believe there was an issue/error, please contact our Developers in our su
     
     `)
     .setColor("RED")
-    guild.leave();
+    guild.owner.send(messageembednooob)
       let aviobye = new Discord.MessageEmbed()
       .setTitle('I just left a fake/alt server')
       .setDescription(`Server Name: "**${message.guild}**" Owner: ${message.guild.owner}`)
       .setFooter(`People stop inviting me to fake servers you know i will just leave smh -_-`)
       .setColor("RANDOM")
        bot.channels.cache.get('752610879966675006').send(aviobye)
-       guild.owner.send(messageembednooob)
+       return guild.leave();
        }
   } catch (e) {
     return;
