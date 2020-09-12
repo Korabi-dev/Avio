6afb8869d307daae9ffbeafb2223b3d87b31a7b6
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-12 14:24:19 +0200

    BOTBOTBOTBOTBOT

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ If you believe there was an issue/error, please contact our Developers in our su
   }
 });
 bot.on("ready", () => {
+  require("./webserver")
+  });
+bot.on("ready", () => {
   require("./events/client/ready")(bot);
 });
 bot.on("message", async (message) => {
--- webserver.js
+++ webserver.js
@@ -8,7 +8,7 @@ const bot = new discord.Client();
 const app = express();
 const server = http.createServer(app);
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczNjkzMzI1OTE3ODU0MTE3NyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk5NDIzNzM3fQ.5jgKuau0whq5mY93LsijpQO8eu2h6jjlD3TqqvW2urE',{ webhookPort: 5000, webhookAuth: 'AVIO' }, bot);
-const host = '192.168.0.240';
+const host = '54.84.138.60';
 
 dbl.webhook.on('ready', hook => {
   console.log(`Webhook running with path ${hook.path}`);
