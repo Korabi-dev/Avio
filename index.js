f29e0683e4909f2c4837bd6106a9780b86845f49
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-15 01:45:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ bot.on('message', message => {
 	}
 });
 bot.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'avio-member-log');
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
   if (!channel) return;
   let welcomembed = new Discord.MessageEmbed()
   .setTitle('New Member!')
