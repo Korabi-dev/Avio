fd0463d83a47355a311e5bd23373cfb5a1ab3f0b
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-16 17:32:41 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -89,24 +89,7 @@ module.exports = async (bot, message) => {
   const cmd = args.shift().toLowerCase();
 
   if (cmd.length === 0) {
-    const Embed = new MessageEmbed()
-        .setTitle(`Help`)
-        .setDescription(
-          `Hey **${message.author.username}** Here are the help commands `
-        )
-        .setColor(`RED`)
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true, size: 256 }))
-        .setFooter(`Note: run the command "a!messagelogs-setup" to see the content of messages in this guild, if you want to undo it run the command "a!messagelogs-delete" `)
-        .addFields(
-          { name: "a!help-moderation", value: `Sends you the moderation commands!`, inline: true },
-          { name: "a!help-info", value: `Sends you the information commands!`, inline: true },
-          { name: "a!help-fun", value: `Sends you the fun commands!`, inline: true },
-          { name: "a!help-utility", value: `Sends you the utility commands!`, inline: true },
-          { name: "a!help-nsfw", value: `Sends you the nsfw commands!`, inline: true },
-          { name: "a!help-music", value: `Sends you the music commands!`, inline: true },
-       );
-      
-     return message.channel.send(Embed);
+    return;
   }
   
 
