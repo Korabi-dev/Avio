208dace6c5bbcc87fe26d6b5122aff174cb22941
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-09-28 17:20:25 +0200

    Update snipe.js

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -5,14 +5,21 @@ module.exports = {
   usage: "[snipe number]",
   category: "fun",
   run: async (bot, message, args) => {
+    
     const snipes = bot.snipes.get(message.channel.id) || [];
     const msg = snipes[args[0] - 1 || 0];
     if (!msg) return message.channel.send(`There is nothing to snipe here!`);
+    if(message.guild.id === '723679963282276384'){
+      if(msg.content.toLowerCase().includes('https://')){
+        return message.channel.send(`This message (snipe) has a link in it and thats not allowed here`)
+      }
+    }
     const Embed = new MessageEmbed()
       .setAuthor(
         msg.author.tag,
         msg.author.displayAvatarURL({ dynamic: true, size: 256 })
       )
+      .setColor("RANDOM")
       .setDescription(msg.content)
       .setFooter(`Date: ${msg.date} | ${args[0] || 1}/${snipes.length}`);
     if (msg.attachment) Embed.setImage(msg.attachment);
