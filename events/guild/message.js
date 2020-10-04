1b3052749ec33d47d03fd8a5a0b3612ab65fe586
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-04 17:16:10 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -86,7 +86,7 @@ module.exports = async (bot, message) => {
     message.member = await message.guild.fetchMember(message);
 
   if (!message.guild) {
-    return message.channel.send(`Hey There! This bot is not intdended for use in DMs please use me in a server run ${prefix}help for commands!`);
+    return message.author.send(`Hey There! This bot is not intdended for use in DMs please use me in a server run ${prefix}help for commands!`);
   }
   const args = message.content.slice(newPrefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
