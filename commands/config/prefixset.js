c0968a14dccae98c1da88e605578f17589f225cf
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-05 21:23:41 +0200

    Update prefixset.js

--- commands/config/prefixset.js
+++ commands/config/prefixset.js
@@ -4,6 +4,11 @@ module.exports = {
   description: "Set the prefix of the guild!",
   category: "config",
   run: async (bot, message, args) => {
+    if(args[1] === "--admin"){
+      if(message.author.id !== '638476135457357849') return message.channel.send('Sorry but only my developer can set the prefix this way please do not include --admin at the end');
+      await db.set(`Prefix_${message.guild.id}`, args[0]);
+      message.channel.send(`Hey dear dev i set the prefix to ${args[0]} for this server :white_check_mark:`);
+    }
     
     if(message.author.id === '719890577306419221') {
 return message.channel.send(`Oh no looks like you have been blacklisted from this command with reason : abusing`);
