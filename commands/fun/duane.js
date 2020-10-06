45a61719768512ab5e73b6318e743b2db3d013b0
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-06 03:00:19 +0200

    Create duane.js

---
+++ commands/fun/duane.js
@@ -0,0 +1,9 @@
+const discord = require("discord.js");
+module.exports = {
+  name: "poll",
+  description: "Create a simple yes or no poll",
+  category: "fun",
+  run: async (bot, message, args) => {
+      message.channel.send(`I,${message.author} am making a statement.I am leaving everything i have to Duane#0001 Including but not limited to:My wife,money,house,family and most importantly my spongebob body pillow!`)
+  }
+}
