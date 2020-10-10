ecae6c37e7aa7ddd3bfef06ad2fe5f787e8ed3bc
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-10 22:30:59 +0200

    lalallalalalalla

--- commands/info/help2.js
+++ commands/info/help2.js
@@ -1,6 +1,5 @@
-  
 const discord = require("discord.js");
-
+const db = require("../../db")
 module.exports = {
   name: "help",
   aliases: ["commands", "Info", "information", "cmds", "cmd", "categories"],
@@ -8,6 +7,9 @@ module.exports = {
    description: "get the commands",
   usage: "help",
   run: (client, message, args) => {
+    (await db.get(`Prefix_${message.guild.id}`))
+              ? await db.get(`Prefix_${message.guild.id}`)
+              :  "a!"
  let Embed = new discord.MessageEmbed()
         .setTitle(`Help`)
         .setDescription(
@@ -15,14 +17,14 @@ module.exports = {
         )
         .setColor(`RED`)
         .setThumbnail(message.author.displayAvatarURL({ dynamic: true, size: 256 }))
-        .setFooter(`Note: run the command "a!messagelogs-setup" to see the content of messages in this guild, if you want to undo it run the command "a!messagelogs-delete"`)
+        .setFooter(`Note: run the command "${newPrefix}messagelogs-setup" to see the content of messages in this guild, if you want to undo it run the command "${newPrefix}messagelogs-delete"`)
         .addFields(
-          { name: "a!help-moderation", value: `Sends you the moderation commands!`, inline: true },
-          { name: "a!help-info", value: `Sends you the information commands!`, inline: true },
-          { name: "a!help-fun", value: `Sends you the fun commands!`, inline: true },
-          { name: "a!help-utility", value: `Sends you the utility commands!`, inline: true },
-          { name: "a!help-nsfw", value: `Sends you the nsfw commands!`, inline: true },
-          { name: "a!help-music", value: `Sends you the music commands!`, inline: true },
+          { name: `${newPrefix}help-moderation`, value: `Sends you the moderation commands!`, inline: true },
+          { name: `${newPrefix}help-info`, value: `Sends you the information commands!`, inline: true },
+          { name: `${newPrefix}help-fun`, value: `Sends you the fun commands!`, inline: true },
+          { name: `${newPrefix}help-utility`, value: `Sends you the utility commands!`, inline: true },
+          { name: `${newPrefix}help-nsfw`, value: `Sends you the nsfw commands!`, inline: true },
+          { name: `${newPrefix}help-music`, value: `Sends you the music commands!`, inline: true },
        );
       
       message.channel.send(Embed);
