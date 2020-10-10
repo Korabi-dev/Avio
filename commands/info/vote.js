ae71f19b9e16fad3a6e391127a52f9c0db2fed07
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-11 00:38:11 +0200

    Update vote.js

--- commands/info/vote.js
+++ commands/info/vote.js
@@ -1,7 +1,5 @@
 const discord = require("discord.js")
 
-const DBL = require("dblapi.js");
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczNjkzMzI1OTE3ODU0MTE3NyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk5NDIzNzM3fQ.5jgKuau0whq5mY93LsijpQO8eu2h6jjlD3TqqvW2urE', bot);
 
 module.exports = {
     name: "vote",
@@ -9,6 +7,10 @@ module.exports = {
      description: "get the commands",
     usage: "help",
     run: (bot, message, args) => {
+     
+
+const DBL = require("dblapi.js");
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczNjkzMzI1OTE3ODU0MTE3NyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTk5NDIzNzM3fQ.5jgKuau0whq5mY93LsijpQO8eu2h6jjlD3TqqvW2urE', bot);
         let user = message.author.id;
         dbl.hasVoted(user).then(voted => {
           if (!voted) {
