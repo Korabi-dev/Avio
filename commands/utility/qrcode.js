cbe290be0b8ff1ab3e9c887392cc30665ffa67c0
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-11 16:30:10 +0200

    Update qrcode.js

--- commands/utility/qrcode.js
+++ commands/utility/qrcode.js
@@ -7,11 +7,15 @@ module.exports = {
     category: "utility",
     aliases: ["genqr", "qrgen"],
     run: async (bot, message, args) => { 
-        request({url: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${args}`, encoding: null}, function(err, res, buffer) {
+        request({url: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${args[0]}`, encoding: null}, function(err, res, buffer) {
 
  let wembed = new Discord.MessageEmbed()
+ .setTitle("QR Code Generated!")
+ .setDescription(`Link: ${args[0]}\nRequested By: ${message.author.username}`)
  .attachFiles([{ name: `Image.png`, attachment: buffer}])
  .setImage('attachment://Image.png')
+ .setColor("RANDOM")
+ .setTimestamp()
  message.channel.send(wembed);
 });
     }
