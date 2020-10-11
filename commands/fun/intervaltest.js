6b5bef676b115da6059e325b2683f892348c545e
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-11 14:04:18 +0200

    Update intervaltest.js

--- commands/fun/intervaltest.js
+++ commands/fun/intervaltest.js
@@ -6,9 +6,12 @@ module.exports = {
     category: "fun",
     run: async (bot, message, args) => { 
         if(message.author.id !== config.OwnerID) return message.channel.send("Only my dev can use this command")
-        setInterval( () => {
+       const int = setInterval( () => {
             bot.channels.cache.get('764805069127548936').send(`Please Don't delete this channel korabi is using it to test (this message will be sent every 5s)`)
-            
+
             }, 5000)
+            if(args.toLowerCase().includes("~~stop")) {
+                clearInterval(int)
+            }
     }
     }
