aa470665a8dc6967ca911d7aaef8035e0a9d97f3
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-11 15:32:09 +0200

    Update qrcode.js

--- commands/utility/qrcode.js
+++ commands/utility/qrcode.js
@@ -8,6 +8,6 @@ module.exports = {
     run: async (bot, message, args) => { 
         const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${args[0]}`
     message.channel.send("Here Is Your QR Code:")
-    message.channel.send(url.replace('https://', '').replace('http://', ''))
+    message.channel.send(url)
     }
 }
