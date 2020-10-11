04cd677709ee06bf770989d6571848c96c21382e
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-11 15:21:57 +0200

    Update qrcode.js

--- commands/utility/qrcode.js
+++ commands/utility/qrcode.js
@@ -1,9 +1,10 @@
 const discord = require("discord.js")
 
 module.exports = {
-    name: "leaveguild",
+    name: "qrcode",
     description: "A role utility command",
     category: "utility",
+    aliases: ["genqr", "qrgen"],
     run: async (bot, message, args) => { 
         const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://${args[0]}`
         const embed = new discord.messageEmbed()
