8caf0d4f4c911bbf6f666295168c11cbd9bda88b
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-12 14:08:18 +0200

    aaaaaaaaaaaaaaaaa

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,9 @@
 const { Message, send } = require("discord.js");
-
+const chalk = require('chalk');
 let config = require('../../config.json')
+
 module.exports = (bot) => {
-  console.log(`${bot.user.username} is now online!`);
+  console.log(chalk.blue(`${bot.user.username} is now online!`));
  
   
   bot.user.setPresence({ activity: { name: `With Avio Developers | ${config.prefix}help | Avio Help` }, status: 'online' })
