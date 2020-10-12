bebc91854e13bdf9ade5e1a9bae91032fb9c6e6c
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-12 18:31:16 +0200

    Update antiswear.js

--- commands/config/antiswear.js
+++ commands/config/antiswear.js
@@ -9,26 +9,25 @@ module.exports = {
     if(!message.member.hasPermission("ADMINISTRATOR")) {
         return message.channel.send(`**${message.author.username}**, You do not have enough permission to use this command (premission needed: Administrator)`)
       }
-      const truee = ("true");
-      const falsee = ("false");
+      
       
   antiswear.findOne(
         { Guild: message.guild.id },
         async (err, data) => {
           if (err) throw err; 
       if (data) {
-        if(data.Value === 'true'){
+        
           data.Value = "false"
           data.save();
 
           message.channel.send(
             `Antiswear Disabled :white_check_mark:`
           );
-        }
+        
         } else if (!data) {
           let newData = new antiswear({
             Guild: message.guild.id,
-            Value: truee,
+            Value: "true",
           });
           newData.save();
           message.channel.send(
