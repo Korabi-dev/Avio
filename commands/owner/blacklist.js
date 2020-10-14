129b39e541545dca61a58e19fb9af7cd6e56d839
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-14 07:32:44 +0200

    aaaaaaaaaa

--- commands/owner/blacklist.js
+++ commands/owner/blacklist.js
@@ -1,4 +1,5 @@
 const blacklist = require("../../models/blacklist");
+const discord = require("discord.js")
 module.exports = {
   name: "blacklist",
   description: "Blacklist a user",
@@ -19,16 +20,24 @@ module.exports = {
         async (err, data) => {
           if (err) throw err; 
       if (data) {
-       return message.channel.send(`User Is Already Blacklisted With Reason : ${data.reason} || User ID ${data.blacklistID}`)
+        const alrembed = new discord.MessageEmbed()
+        .setTitle("Blacklist!")
+       .setDescription(`User Is Already Blacklisted With Reason : ${data.reason}`)
+       .setFooter(`User ID: ${data.blacklistID}`)
+       .setColor("RED")
+       return message.channel.send(alrembed)
           } else if (!data) {
           let newData = new blacklist({
             blacklistID: id,
             reason: reason2,
           });
           newData.save();
-          message.channel.send(
-            `User Blacklisted Reason: ${reason2}`
-          );
+          const newembed = new discord.MessageEmbed()
+          .setTitle("Blacklisted!")
+          .setDescription(`${target} (${target.user.tag}) has been blacklisted\nReason: ${reason2}\nID: ${id}`)
+          .setTimestamp()
+          .setColor("GREEN")
+          return message.channel.send(newembed)
         }
     });
       }
