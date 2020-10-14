9ed225b126ab564e81f87c0685d96ab2564fe181
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-14 21:02:10 +0200

    Update commandinfo.js

--- commands/info/commandinfo.js
+++ commands/info/commandinfo.js
@@ -6,11 +6,15 @@ module.exports = {
      description: "get the info for a command!",
      run: async (bot, message, args) => {
         const command = bot.commands.get(args[0]);
+        let aliases = bot.commands.get(bot.aliases.get(args[0]));
         if(!args[0]) return message.channel.send(":x: | Please Provide a command name!")
         if(!command) return message.channel.send(`:x: | Could not find the command \"${args[0]}\"`)
         let name = command.name;
         let description = command.description;
         let usage = command.usage;
+        if(!aliases){
+            aliases = "No Aliases Found!";
+        }
        if(!description) {
            description = "No Destiption Found";
        }
@@ -19,7 +23,7 @@ module.exports = {
        }
        const embed = new discord.MessageEmbed()
        .setTitle("Command Info!")
-       .setDescription(`Command Name: ${name}\nCommand Description: ${description}\nCommand Usage: ${usage}`)
+       .setDescription(`Command Name: ${name}\nCommand Aliases: ${aliases}\nCommand Description: ${description}\nCommand Usage: ${usage}`)
        .setColor("BLUE")
        .setTimestamp()
        return message.channel.send(embed)
