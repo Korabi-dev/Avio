6342ad2062c01e9d9fa223df4182f2af1db4607e
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-14 11:55:09 +0200

    Create trash.js

---
+++ commands/image/trash.js
@@ -0,0 +1,15 @@
+const canvacord = require("canvacord");
+const discord = require("discord.js")
+module.exports = {
+    name: 'trash',
+    run: async (bot, message, args) => {
+        let target =   message.mentions.members.first();
+        let avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
+        if(target) {
+            avatar = target.user.displayAvatarURL({ dynamic: false, format: 'png' });
+        }
+        let image = await canvacord.Canvas.trash(avatar);
+        let attachment = new discord.MessageAttachment(image, "triggered.gif");
+        return message.channel.send(attachment);
+    }
+}
