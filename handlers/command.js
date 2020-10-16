26fe18df04b13934ed2005046488ed67d0abfd0f
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-16 11:31:34 +0200

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,5 +1,7 @@
   const discord = require("discord.js")
-  
+  const ascii = require('ascii-table')
+  let table = new ascii("Commands");
+  table.setHeading('Command', ' Load status');
 const { readdirSync } = require("fs");
 const { join } = require("path");
 module.exports = (bot) => {
@@ -8,11 +10,12 @@ module.exports = (bot) => {
   readdirSync("./commands/").map((dir) => {
     const commands = readdirSync(`./commands/${dir}/`).map((cmd) => {
       let pull = require(`../commands/${dir}/${cmd}`);
-      console.log(`Loaded command ${pull.name}!`);
+      table.addRow(file,'✅')
       bot.commands.set(pull.name, pull);
       if (pull.aliases) {
         if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
       }
     });
   });
+  console.log(table.toString());
 };
