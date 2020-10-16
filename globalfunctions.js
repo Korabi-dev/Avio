345a09df2d298540eefbf4fa711b6c0a1c0ba521
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-16 16:03:49 +0200

    Update globalfunctions.js

--- globalfunctions.js
+++ globalfunctions.js
@@ -1,13 +1,8 @@
-const Timeout = new Set();
 const { MessageEmbed, Message, Client } = require("discord.js");
 const discord = require("discord.js");
-const { prefix } = require("../../config.json");
+const config = require("../config.json");
 const ms = require("ms");
-const db = require("../../db");
-const custom = require("../../models/custom");
-const antiswear = require("../../models/antiswear")
-const Filter = require('bad-words');
-const filter = new Filter({ placeHolder: '#'});
+
 /**
  * @param {Client} bot
  * @param {Message} message
