f81ebc443319e33d7316c91d25d779f7fb7ec280
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-16 22:36:21 +0200

    aa

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -21,7 +21,7 @@ module.exports = {
     name: 'eval',
     description: "this command is dev only so i wont show any desription!",
     run: async(bot, message, args) => {
-        const owners = ["638476135457357849", "661676670482251796"]
+        const owners = ["638476135457357849"]
         if(!owners.includes(message.author.id)) return message.channel.send(":x: || This Command Is For My developer Only!")
     if(!args[0]){
     return message.channel.send(":x: | No Code Found.")
