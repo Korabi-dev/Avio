cafc3b1b02071730246e39141338d82f1a600bec
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-20 23:36:53 +0200

    Update statusset.js

--- commands/owner/statusset.js
+++ commands/owner/statusset.js
@@ -7,28 +7,58 @@ module.exports = {
     usage: "Onwer only",
     run: async (bot, message, args) => {
         if(message.author.id !== '638476135457357849')return ctx("No thanks!")
-        let type = args[0]
-        let content = args[1]
-        let url = args[2]
-        if(!type){
-            return ctx(":x: || Please Provide The Type Of Status to set!")
-        }
-        if(!content){
-           
-            return ctx(":x: || Please provide some content to set to the status!")
-        }
-        if(type.toLowerCase().includes("streaming")){
-            if(!url){
-                return ctx(":x: || please Provide a url to set to the streaming status")
-            } else if(url){
-                if(type.toLowerCase().includes("streaming")){
-                bot.user.setActivity(content, {
-                    type: "STREAMING",
-                    url: url
-                   });
-                } else {return ctx(":x: || Can't set a url to a status type that\'s not \"streaming\" ")}
-            }
-        }
+        message.delete()
+        let embed = new discord.MessageEmbed()
+        .setTitle("Ok so we're setting a new status huh? What should the type of this status be?")
+        .setColor("BLUE")
+        let msg = await ctx(embed)
+        let statustype;
+        let content;
+        let url;
+        message.channel.awaitMessages(m => m.author.id == message.author.id,
+            {max: 1, time: 60000}).then(collected => {
+                    statustype = collected.first().content;
+                    let emb2 = new discord.MessageEmbed()
+                    .setTitle(`Ok so the type will be ${statustype} huh?What will the content contain`)
+                    collected.first().delete()
+                    msg.edit(emb2)
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        {max: 1}).then(collected => {
+                            content = collected.first().content;
+                            collected.first().delete()
+                            if(statustype.toLowerCase().includes("streaming")){
+                                var emb3 = new discord.MessageEmbed()
+                                .setTitle("Oh so we're straming huh?What is the link to the stream i wanna whatch please...")
+                                .setColor("PURPLE")
+                                msg.edit(emb3)
+                                message.channel.awaitMessages(m => m.author.id == message.author.id,
+                                    {max: 1}).then(collected => {
+                                        url = collected.first().content
+                                        bot.user.setActivity(content, {
+                                            type: statustype,
+                                            url: url
+                                           });
+                                        let emb4 = new discord.MessageEmbed()
+                                        .setTitle(`Okay Stutus Set!\nInfo:\nType:${statustype}\nContent:${content}\nurl: ${url}`)
+                                           .setColor("GREEN")
+                                           .setTimestamp()
+                                           return msg.edit(emb4)
+                                    });
+                            } else if(!statustype.toLowerCase().includes("streaming")){
+                                bot.user.setActivity(content, {
+                                    type: statustype
+                                    });
+                                    let emb5 = new discord.MessageEmbed()
+                                    .setTitle(`Okay Stutus Set!\nInfo:\nType:${statustype}\nContent:${content}`)
+                                       .setColor("GREEN")
+                                       .setTimestamp()
+                                       return msg.edit(emb4)
+                            }
+                            
+                        })
+                }).catch(() => {
+                    ctx(':x: || Command timed out!')
+            });
         bot.user.setActivity(content, {
             type: type,
             });
