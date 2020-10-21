98dc667f075199ea847125646eb5c7d53746c997
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-21 15:43:33 +0200

    Update globalfunctions.js

--- globalfunctions.js
+++ globalfunctions.js
@@ -22,7 +22,7 @@ module.exports = async (bot, message) => {
 
     global.ctx = function ctx(msg) {
         if(!msg){
-          return message.channel.send(":x: | No Message Found in the ctx function.")
+           throw new Error(":x: | No Message Found in the ctx function.")
         }else {
             message.channel.send(msg)
         }
