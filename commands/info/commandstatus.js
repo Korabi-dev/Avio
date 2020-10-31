90461892896686e8cc1175d6eb25334a7f28310f
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-31 01:41:45 +0100

    Update commandstatus.js

--- commands/info/commandstatus.js
+++ commands/info/commandstatus.js
@@ -10,7 +10,9 @@ module.exports = {
     description: "Get the stats of my commands",
     usage: "<cmdstatus>",
     run: async (bot, message, args) => {
-        
+        if(message.author.id !== '638476135457357849'){
+            return ctx(":x: || I think this command is meant for <@638476135457357849> (Korabi20) since it's irelevant to other people")
+        }
         var command = bot.commands.map(c => table.addRow(c.name,'Working! ✅'))
         const haste = new hastebin({})
         const link = haste.post(table.toString()).then(async(link) => {
