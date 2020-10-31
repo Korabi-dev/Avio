cd9600b1e590a5de004d1cfb335c955d9e9d25fa
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-10-31 01:28:53 +0100

    Update commandstatus.js

--- commands/info/commandstatus.js
+++ commands/info/commandstatus.js
@@ -1,13 +1,18 @@
-const ascii = require('ascii-table')
+const ascii = require('ascii-table');
+const { DiscordAPIError } = require('discord.js');
 let table = new ascii("Commands");
 table.setHeading('Command', 'Status');
+const discord = require("discord.js")
+const hastebin = require("hastebin.js")
 module.exports = {
     name: "cmdstatus",
     category: "info",
     description: "Get the stats of my commands",
     usage: "<cmdstatus>",
     run: async (bot, message, args) => {
+        const embed = new DiscordAPIError.MessageEmbed()
         var command = bot.commands.map(c => table.addRow(c.name,'Working! ✅'))
-       ctx(table.toString()) 
+        const haste = new hastebin({})
+        const link = haste.post(table.toString()).then(async(link) => {ctx("Here Is The Command Status"+ " " + link)});
     }
 }
