5d751931e0c14130a0dd741791196ab89faa874d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-06 00:19:21 +0100

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,12 +1,18 @@
 module.exports = {
   name: "say",
   description: "Get the bot to say what ever you want!",
-  usage: "<msg>",
+  usage: "<message>",
   run: async (bot, message, args) => {
-    if (!message.member.permissions.has("MANAGE_MESSAGES")) return;
-    let MSG = message.content.split(`${bot.prefix}say `).join("");
-    if (!MSG)
-      return message.channel.send(`You did not specify the message to send!`);
+    if (!message.member.permissions.has("MANAGE_MESSAGES")) return ctx("You do not have enough permissions to use this command.");
+    let MSG = args.slice(1).join(" ");
+    if (!MSG) return message.channel.send(`You did not specify the message to send!`);
+    if(bot.owners.includes(message.author.id)){
+      message.channel.send(MSG);
+      return message.delete();
+    }
+    if(message.mentions.roles.first() && !message.member.permissions.has("MANAGE_MESSAGES")){
+      return ctx("You can't mention a role unless you can mention everyone by default.")
+    }
     message.channel.send(MSG);
     message.delete();
   },
