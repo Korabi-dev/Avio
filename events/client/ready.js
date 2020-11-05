14d6d30f8897e55325e02a35ea21729bee95e11d
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-05 17:00:26 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,5 +3,5 @@ const chalk = require('chalk');
 let config = require('../../config.json')
 
 module.exports = (bot) => {
-  bot.user.setPresence({ activity: { name: `With Avio Developers | ${config.prefix}help | Avio Help` }, status: 'online' })
+  bot.user.setPresence({ activity: { name: `With My Developer | ${config.prefix}help | ${bot.guilds.cache.size} Servers! ` }, status: 'online' })
   };
