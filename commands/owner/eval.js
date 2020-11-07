b13b3534231d84397948176595b9f6cd068a84f8
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-07 18:53:53 +0100

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -23,10 +23,10 @@ const hastebin = require("hastebin.js");
 
 module.exports = {
     name: 'eval',
+    owneronly: true,
     description: "this command is dev only so i wont show any desription!",
     run: async(bot, message, args) => {
-        const owners = ["638476135457357849", "661676670482251796"]
-        if(!owners.includes(message.author.id)) return message.channel.send(":x: || This Command Is For My developer Only!")
+        
     if(!args[0]){
     return message.channel.send(":x: | No Code Found.")
     }  
