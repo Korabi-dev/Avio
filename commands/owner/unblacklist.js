e0d64f293b01de912bef316cac5baf3e917446e2
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-07 21:16:28 +0100

    updated some stuff

--- commands/owner/unblacklist.js
+++ commands/owner/unblacklist.js
@@ -3,12 +3,11 @@ const discord = require("discord.js")
 module.exports = {
   name: "unblacklist",
   description: "unBlacklist a user (dev only)",
+  owneronly: true,
   category: "config",
   aliases: ["unbl"],
   run: async (bot, message, args) => {
-    const owners = ["638476135457357849", "661676670482251796", "362332196016750592"]
-    if(!owners.includes(message.author.id)) return message.channel.send(":x: || This Command Is For My developer Only!")
-   let target =   message.mentions.members.first() ||
+    let target =   message.mentions.members.first() ||
    message.guild.members.cache.get(args[0]);
    if(!target) return message.channel.send(`:x: || Please mention who to unblacklist <mention/id>`);
    const id = target.user.id || args[0]
