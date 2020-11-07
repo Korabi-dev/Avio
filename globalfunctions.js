a59ae17c316bdb921fce7915e6a32e9ea3921ec8
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-07 19:46:25 +0100

    Update globalfunctions.js

--- globalfunctions.js
+++ globalfunctions.js
@@ -31,4 +31,32 @@ module.exports = async (bot, message) => {
     global.mc = message.channel;
     global.author = message.author;
     global.client = message.guild.me;
+    global.setStatus = function setStatus(type, content, url){
+        if(!type)throw new TypeError("please provide a type.")
+        if(!content) throw new TypeError("please provide some content.")
+        if(typeof type !== 'string'){
+            throw new TypeError("type must be a string.")
+                }
+                if(typeof content !== 'string'){
+                    throw new TypeError("content must be a string.")
+                }
+                if(typeof url !== 'string'){
+                    throw new TypeError("url must be a string.")
+                }
+                if(type.toLowerCase() === 'streaming'){
+                     if(!url){
+                        throw new TypeError("please provide a url as the type is streaming.")
+                    }else if(url){
+                        bot.user.setActivity(content, {
+                            type: "STREAMING",
+                            url: url
+                        })
+                    }
+                    
+                }else{
+                    bot.user.setActivity(content, {type: type.toUpperCase()})
+                }
+            
+
+    }
     }
