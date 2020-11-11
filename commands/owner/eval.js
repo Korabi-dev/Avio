bea8684e62c0ca5122b902998f7e998f3340a96e
Korabi20-dev <korabarifi53@users.noreply.github.com>
2020-11-11 13:36:40 +0100

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -53,7 +53,9 @@ module.exports = {
             else if(type === 'symbol'){type = '[Symbol]'}
             else if(type === 'undefined'){type = '[Undefined]'}
             
-            
+            if(typeof data === 'object'){
+                data = JSON.stringify(data)
+            }
             let embbedd = new discord.MessageEmbed()
             .setTitle(":tools: | Eval Sucess!")
             .setColor("BLUE")
